29dd601c856d48c72e38f8c8d1bd40f13a276aa5
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-01-14 19:55:42 +0530

    Add API module with Axios configuration

---
+++ frontend/src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://YOUR_RENDER_BACKEND_URL",
+});
+
+api.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
+
+export default api;
